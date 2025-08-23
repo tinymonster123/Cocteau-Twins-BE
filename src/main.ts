66c030263ad13c852bfbf0a86bea22f7ae42ef7c
tinymonster123 <lfhxgs123@gmail.com>
2025-08-24 05:26:23 +0800

    feat: 初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
+const bootstrap = async () => {
+    const logger = new Logger();
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    logger.log(`Application listening on port ${port}`);
+};
 bootstrap();
