7249a38741ebd3157a3a00c65265f4169503dff0
tinymonster123 <lfhxgs123@gmail.com>
2025-08-25 03:46:31 +0800

    refactor: 调整 auth 文件夹路径

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,40 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User } from 'src/users/users.entity';
+import { AccessToken } from './types/AccessToken';
+import { UsersService } from 'src/users/users.service';
+import { RegisterRequestDto } from './dtos/register-request.dto';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService,
+    ) { }
+    async validateUser(email: string, password: string): Promise<User> {
+        const user: User = await this.usersService.findOneByEmail(email);
+        if (!user) {
+            throw new BadRequestException('User not found');
+        }
+        const isMatch: boolean = bcrypt.compareSync(password, user.password);
+        if (!isMatch) {
+            throw new BadRequestException('Password does not match');
+        }
+        return user;
+    }
+    async login(user: User): Promise<AccessToken> {
+        const payload = { email: user.email, id: user.id };
+        return { access_token: this.jwtService.sign(payload) };
+    }
+    async register(user: RegisterRequestDto): Promise<AccessToken> {
+        const existingUser = await this.usersService.findOneByEmail(user.email);
+        if (existingUser) {
+            throw new BadRequestException('email already exists');
+        }
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        const newUser: User = { ...user, password: hashedPassword };
+        await this.usersService.create(newUser);
+        return this.login(newUser);
+    }
+}
