4b27616848138ae3d67bdfc3b2c1a92cb095ee81
tinymonster123 <lfhxgs123@gmail.com>
2025-08-26 04:56:16 +0800

    feat: 配置 openapi

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
 
 const bootstrap = async () => {
     const logger = new Logger();
     const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('Cocteau Twins API')
+        .setDescription('The Cocteau Twins API description')
+        .setVersion('1.0')
+        .addTag('cocteau twins')
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, documentFactory);
     app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
     const port = process.env.PORT || 3000;
