fa2eb70f93d586b8c4334b27aa37c62ed7941270
tinymonster123 <lfhxgs123@gmail.com>
2025-08-25 22:10:44 +0800

    feat: 定义各个功能模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AuthModule } from "./users/auth/auth.module";
+import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-    imports: [AuthModule, UsersModule]
+    imports: [AuthModule, UsersModule, PrismaModule]
 })
 
 export class AppModule { }
