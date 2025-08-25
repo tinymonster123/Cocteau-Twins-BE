6c0840d5aa57c6d83df43128807dee6ab4e0cc2d
tinymonster123 <lfhxgs123@gmail.com>
2025-08-26 03:54:35 +0800

    feat: 为每个 endpoint 添加守卫

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { JwtGuard } from './auth/guards/jwt.guard';
 
 @Module({
-    imports: [AuthModule, UsersModule, PrismaModule]
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        AuthModule,
+        UsersModule,
+        PrismaModule
+    ],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: JwtGuard,
+        },
+    ],
 })
-
 export class AppModule { }
