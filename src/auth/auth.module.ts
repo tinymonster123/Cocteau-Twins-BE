2c1017d28fd3aacc2bd4c6e2ffc1a29357c4f38c
tinymonster123 <lfhxgs123@gmail.com>
2025-08-26 13:53:29 +0800

    chore: 删除未使用的导入

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { LocalStrategy } from './strategy/local.strategy';
+
 import { JwtStrategy } from './strategy/jwt.strategy';
 import { RefreshTokenService } from './refresh-token.service';
+import { RolesGuard } from './guards/roles.guard';
 import { UsersModule } from '../users/users.module';
 import { PrismaModule } from '../prisma/prisma.module';
 
@@ -24,8 +25,8 @@ import { PrismaModule } from '../prisma/prisma.module';
     UsersModule,
     PrismaModule,
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy, RefreshTokenService],
+  providers: [AuthService, JwtStrategy, RefreshTokenService, RolesGuard],
   controllers: [AuthController],
   exports: [AuthService],
 })
-export class AuthModule { }
+export class AuthModule {}
